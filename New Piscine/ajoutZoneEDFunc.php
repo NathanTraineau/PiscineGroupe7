8b554a5d109cef94948f2333df5a4952b22c688f
ines-missoum <32544041+ines-missoum@users.noreply.github.com>
2018-01-13 21:57:27 +0100

    ajoutZoneEDfunc

--- New Piscine/ajoutZoneEDFunc.php
+++ New Piscine/ajoutZoneEDFunc.php
@@ -4,20 +4,19 @@
 		//connexion base de donnees
 		$mybdd = new PDO('mysql:host=localhost;dbname=piscine', 'root', '');
 		$editeur = $_POST['numEditeur'];
-		//$sql="SELECT NumEditeur, NomEditeur FROM `editeur` WHERE NumEditeur='$editeur' ";
-		$sql =	' SELECT NumEditeur, NomEditeur FROM editeur WHERE NumEditeur='$editeur' ';	
+		//on récupère le nom de l'editeur
+		$sql =	' SELECT NumEditeur, NomEditeur FROM editeur WHERE NumEditeur=\'' . $editeur . '\'';	
 		$reponse = $mybdd->query($sql);
-		
-		//print_r($reponse);
-		//$ajout="INSERT INTO zone (NumZone, NomZone) VALUES (NULL, '$nomEditeur')";
-		//$requete1= $mybdd->prepare('INSERT INTO zone (NumZone, NomZone) VALUES (NULL, '$editeur')');
-		//$requete1->execute(array('nomZone'=>$_POST['NameZone']));
-		//$requete2= $mybdd->prepare('INSERT INTO regrouper (IdRegrouper, NumEditeur, NumZone) VALUES (NULL, :numEditeur, :numZone)');
-		//$requete2->execute(array('nomZone'=>$_POST['NameZone']));
+		$reponse = $reponse ->fetch();
+		$nomEditeur=$reponse['NomEditeur'];
+		// on crée une zone à son nom
+		$requete1= $mybdd->prepare('INSERT INTO zone (NumZone, NomZone) VALUES (NULL, :ed)');
+		$requete1->execute(array('ed'=>$nomEditeur));
+	
 	?>
 
-	<!-- RETOUR 
+	<!-- RETOUR -->
 	
-	<script type="text/javascript">location.href = 'zone.php';</script> -->
+	<script type="text/javascript">location.href = 'zone.php';</script> 
 
 </html>
