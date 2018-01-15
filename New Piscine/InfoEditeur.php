b2075295e0ec98af90dd93e33c0c95ad9c5760e2
CiubucAlexandra <ciubucalexandra@yahoo.com>
2018-01-15 03:40:07 +0200

    infoJeux

--- New Piscine/InfoEditeur.php
+++ New Piscine/InfoEditeur.php
@@ -13,11 +13,6 @@
 
 <?php
 	include'inc/header.php';
-    //$servername = "localhost";
-    //$username = "root";
-    //$password = "";
-    //$dbname = "piscine";
-    //$editeur= "editeur";
     if ( !empty($_POST['infoID'])) {
     		$num = $_POST['infoID']; // si ça vient du bouton info on aura la variable infoID en post mais si ça vient de la barre recherche alors c'est la varia nomEditeur qu'on va traduire en NumEditeur juste en dessous.
     		} 
@@ -38,10 +33,11 @@
     $q = $myPDO->query($sql2);
     $NomEdit = $myPDO->query($edit)->fetch();
     ?>
+<div class="container">
     <form method="POST" action="editeur.php">
     <button type="submit">Retour Editeurs</button>
 	</form>
-    <table class="table table-bordered table-condensed">
+    <table class="table table-bordered table-condensed" id="contact">
         <thead>
         	<h3>Contacts de <?php echo $NomEdit['NomEditeur'] ?></h3>
             <tr>
@@ -49,7 +45,8 @@
                 <th>Prenom Contact</th>
                 <th>Numéro Contact</th>
                 <th>Email du Contact</th>
-                <th></th>
+                <th>Actions</th>
+              
             </tr>
         </thead>
         <tbody>
@@ -83,7 +80,7 @@
     </table>
     <form method="POST" action="AjoutContact.php">
         <input type="hidden" name="infoID" value="<?php echo $num; ?>" />
-		<button type="submit">Ajouter un contact</button>
+		<button class="button" type="submit">Ajouter un contact</button>
 	</form>
 
 <!-- On affiche maintenant ses jeux
@@ -95,49 +92,124 @@
 
 	?>
 	
-	<table class="table table-bordered table-condensed" text-align="center">
+	<table class="table table-bordered table-condensed" text-align="center" id="jeux">
         <thead>
         	<h3>Jeux de <?php echo $NomEdit['NomEditeur'] ?></h3>
-            
+             <tr>
+                    <th>Nom du jeux </th>
+                    <th>Actions</th>
+            </tr>
         </thead>
         <tbody>
             <?php while ($JeuxCourant = $jeux->fetch()): ?>
+                    <!--<td><?php echo htmlspecialchars($JeuxCourant['NomJeux']) ?></td>-->
                 <tr>
-                    <tr><td> Nom du jeux :</td><td><strong><?php echo htmlspecialchars($JeuxCourant['NomJeux']) ?><strong></td> 
-                    	<td>  
-                    	<form method="POST" action="supJeux.php">
+                    <td><?php echo htmlspecialchars($JeuxCourant['NomJeux']);?></td>
+                    
+                    <td>  
+                        <form method="POST" action="supJeux.php">
                             <input type="hidden" name="jeuxID" value="<?php echo $JeuxCourant['NumJeux']; ?>">
                             <input type="hidden" name="infoID" value="<?php echo $num; ?>" />
-                            <input type="submit" style="float:right;" id="suppr" value="Suppr" />
+                            <input type="submit" style="float:right;" id="suppr" value="Supprimer" />
                         </form>
-                    	</td>
-                    	<td>
+                        
+                        
                         <form method="POST" action="modifJeux.php">
                             <!--<button type="submit">Modif</button> -->
                             <input type="hidden" name="jeuxID" value="<?php echo $JeuxCourant['NumJeux']; ?>" />
                             <input type="hidden" name="infoID" value="<?php echo $num; ?>" />
-                            <input type="submit" style="float:right;" id="suppr" value="modif" />
+                            <input type="submit" style="float:right;" id="suppr" value="Modifier" />
+                        </form> 
+
+                         <form method="POST" action="InfoJeux.php">
+                            <!--<button type="submit">Modif</button> -->
+                            <input type="hidden" name="infoID" value="<?php echo $JeuxCourant['NumJeux']; ?>" />
+                            <input type="hidden" name="editeurID" value="<?php echo $num; ?>" />
+                            <input type="submit" style="float:right;" id="suppr" value="Info" />
                         </form> 
                         </td> 
-                    </tr>
-                    <tr><td>Nombre de Joueurs : <td><?php echo htmlspecialchars($JeuxCourant['NombreJoueur']); ?></td></tr>
-                    <tr><td>Date de Sortie : <td><?php echo htmlspecialchars($JeuxCourant['DateSortie']); ?></td></tr>
-                    <tr><td>Duree Partie<td><?php echo htmlspecialchars($JeuxCourant['DureePartie']); ?></td></tr>
-                    <tr><td>Categorie penser a faire une fonction qui renvoie le str de la caté<td><?php echo htmlspecialchars($JeuxCourant['CodeCategorie']); ?></td></tr>
-                    <tr>
-                        
-                        <!-- <form method="POST" action="modifEditeur.php"> 
-                            
-                            <input type="submit" style="float:right;" id="modif" value="Modif"/>Modif</button>
-                        </form>-->
-                    </td>
                 </tr>
-            </br>
+                
             <?php endwhile; ?>
     
         </tbody>
     </table>
     <form method="POST" action="ajoutJeux.php">
         <input type="hidden" name="infoID" value="<?php echo $num; ?>" />
-		<button type="submit">Ajouter un jeux</button>
+		<button class="button" type="submit">Ajouter un jeux</button>
 	</form>
+</div>
+<style>
+#jeux {
+    font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+}
+#jeux th {
+    border: 1px solid #ddd;
+    padding: 8px;
+}
+
+#jeux tr:nth-child(even){background-color: #f2f2f2;}
+
+#jeux tr:hover {background-color: #ddd;}
+
+#jeux th {
+    padding-top: 12px;
+    padding-bottom: 12px;
+    text-align: left;
+    background-color: #4CAF50;
+    color: white;
+
+}
+#contact {
+    font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+}
+#contact th {
+    border: 1px solid #ddd;
+    padding: 8px;
+}
+
+#contact tr:nth-child(even){background-color: #f2f2f2;}
+
+#contact tr:hover {background-color: #ddd;}
+
+#contact th {
+    padding-top: 12px;
+    padding-bottom: 12px;
+    text-align: left;
+    background-color: #4CAF50;
+    color: white;
+
+}
+.button {
+    background-color: #4CAF50; /* Green */
+    border: none;
+    color: white;
+    padding: 10px 15px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    -webkit-transition-duration: 0.4s; /* Safari */
+    transition-duration: 0.4s;
+    cursor: pointer;
+}
+
+.button {
+    background-color: white; 
+    color: black; 
+    border: 2px solid #4CAF50;
+}
+
+.button:hover {
+    background-color: #4CAF50;
+    color: white;
+}
+
+
+    </style>
+</html>
